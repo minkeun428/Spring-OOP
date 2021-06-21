93624f5b23fc152aa03fd41e22b7dd1626ac18d5
minkeun <minkeun428@gmail.com>
2021-06-21 22:52:48 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,16 +12,26 @@ import hello.core.order.OrderServiceImpl;
 /**
  * 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성" 한다.
  * 생성한 객체 인스턴스의 참조를 "생성자를 통해서 주입(연결)" 해준다.
+ * 메서드를 통해 역할과 구현 클래스가 한눈에 보인다.
  * */
 public class AppConfig {
 
-    // 생성자 주입
+    // 메서드를 주입
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // 메서드가 구현
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
