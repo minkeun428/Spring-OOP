13a68d46fe23f145e19757754dcc31b02cc6f869
minkeun <minkeun428@gmail.com>
2021-06-22 00:13:14 +0900

    스프링 컨테이너 사용 -> @Configuration, @Bean 추가

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 공연 기획자 역할
@@ -15,22 +17,28 @@ import hello.core.order.OrderServiceImpl;
  * 생성한 객체 인스턴스의 참조를 "생성자를 통해서 주입(연결)" 해준다.
  * 메서드를 통해 역할과 구현 클래스가 한눈에 보인다.
  * */
+@Configuration
 public class AppConfig {
 
+
     // 메서드를 주입
+    @Bean   // IoC컨테이너에 등록
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // 메서드가 구현
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,18 +3,25 @@ package hello.core;
 import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        //이전 객체 주입
-        //MemberService memberService = new MemberServiceImpl();
-        //OrderService orderService = new OrderServiceImpl();
+//        이전 객체 주입
+//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
         
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
